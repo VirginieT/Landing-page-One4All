032861dfdeeefaf10bc4a13e6fa4f72fdde82e80
Vynie <vtouzalin33@gmail.com>
2022-03-22 22:34:26 +0100

    animation mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,36 +1,28 @@
 const btnHamburger = document.querySelector('#btnHamburger');
-// const body = document.querySelector('body');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
-// const overlay = document.querySelector('.overlay');
-// const fadeElems = document.querySelectorAll('.has-fade');
+const overlay = document.querySelector('.overlay');
+const fadeElems = document.querySelectorAll('.faded');
 
 btnHamburger.addEventListener('click', function(){
   console.log('click hamburger');
 
-  if(header.classList.contains('open')){ 
-    header.classList.remove('open'); 
-  }
-  else { 
-    header.classList.add('open');
-  }
-});
-
-//   if(header.classList.contains('open')){ // Close Hamburger Menu
-//     body.classList.remove('noscroll');
-//     header.classList.remove('open');    
-//     fadeElems.forEach(function(element){
-//       element.classList.remove('fade-in');
-//       element.classList.add('fade-out');
-//     });
+  if(header.classList.contains('open')){ // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');    
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
     
-//   }
-//   else { // Open Hamburger Menu
-//     body.classList.add('noscroll');
-//     header.classList.add('open');
-//     fadeElems.forEach(function(element){
-//       element.classList.remove('fade-out');
-//       element.classList.add('fade-in');
-//     });
+  }
+  else { // Open Hamburger Menu
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElems.forEach(function(element){
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
 
-//   }  
-// 
+  }  
+});
